9e795949a4ad27061603047996f7ca5ff3b2bbd7
Robert Marik <robert.marik.cz@gmail.com>
2020-05-13 20:12:07 +0200

    testy

--- uis/printacroweb.pl
+++ uis/printacroweb.pl
@@ -1,17 +1,17 @@
 
-print  "\\errorstopmode";
-print  "\\documentclass{article}\n\\usepackage{amsmath,amsfonts}\n\\usepackage{multicol}\n\\usepackage{a4wide}\n\\usepackage[czech]{babel}\\usepackage[IL2]{fontenc}\\usepackage[latin2]{inputenc}\n\\begin{document}\n";
-print  "\\newcount\\cislo\\everymath{\\displaystyle}\n";
-print  "\\def\\correct#1{\\fbox{\$\\bullet\$} {#1}\\\\ }\n";
-print  "\\def\\incorrect#1{\\fbox{\$\\times\$} {#1}\\\\ } \n";
-print  "\\parindent 0pt\n";
-print  "\\parskip 10pt\n";
-print  "\\rightskip 0 pt plus 1 fill\n";
-print  "\\columnseprule 0.4pt\n";
+# print  "\\errorstopmode";
+# print  "\\documentclass{article}\n\\usepackage{amsmath,amsfonts}\n\\usepackage{multicol}\n\\usepackage{a4wide}\\usepackage{graphicx}\n\\usepackage[czech]{babel}\\usepackage[IL2]{fontenc}\\usepackage[latin2]{inputenc}\n\\begin{document}\n";
+# print  "\\newcount\\cislo\\everymath{\\displaystyle}\n";
+# print  "\\def\\correct#1{\\fbox{\$\\bullet\$} {#1}\\\\ }\n";
+# print  "\\def\\incorrect#1{\\fbox{\$\\times\$} {#1}\\\\ } \n";
+# print  "\\parindent 0pt\n";
+# print  "\\parskip 10pt\n";
+# print  "\\rightskip 0 pt plus 1 fill\n";
+# print  "\\columnseprule 0.4pt\n";
 
-print  "\\def\\jednicka{1 }\n";
-print  "\\def\\sloupce{2 }";
-print  "\\ifx\\sloupce\\jednicka \\else \\begin{multicols}{\\sloupce} \\fi\n";
+# print  "\\def\\jednicka{1 }\n";
+# print  "\\def\\sloupce{2 }";
+# print  "\\ifx\\sloupce\\jednicka \\else \\begin{multicols}{\\sloupce} \\fi\n";
 
 @pole=<STDIN>;
 
@@ -39,15 +39,15 @@ foreach $radek (@pole)
 	    }
 	    else
 	    {
-		print  "\$\\global\\advance\\cislo by 1\\textbf{\\fbox{\\the \\cislo}}\$ ",$radek,"\\\\ ";
+		print  "\\mezera\$\\global\\advance\\cislo by 1\\textbf{\\fbox{\\the \\cislo}}\$ ",$radek,"\\\\ ";
 	    }
 	}
     }
 }  
 
 
-print  "\\ifx\\sloupce\\jednicka\\else\\end{multicols}\\fi\n";
-print  "\\end{document}";
+#print  "\\ifx\\sloupce\\jednicka\\else\\end{multicols}\\fi\n";
+#print  "\\end{document}";
 
 
 
